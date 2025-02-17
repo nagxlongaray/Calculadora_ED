8895ad2fe2236ea6069d9fc07f2d089d05de27d0
Nagore Longaray <nagore.longaray098@comunidadunir.net>
2025-02-17 19:51:57 +0100

    ADD: Creamos y probamos ClienteMain con menu de clase Suma

--- src/cliente/ClienteMain.java
+++ src/cliente/ClienteMain.java
@@ -1,7 +1,10 @@
 package cliente;
 
+import javabean.Suma;  // Asegúrate de que esta línea esté correcta
+
 public class ClienteMain {
+    public static void main(String[] args) {
 
-
-
+        Suma.menu();  // Llamamos al metodo estático menu() de la clase Suma
+    }
 }
